a37ac1a0d59a6db68b6f91f2c6653b14b1c1ec61
Sangho Lee (Stan) <87916237+slee8495@users.noreply.github.com>
2024-08-30 02:35:30 -0700

    Update news_advisor_bot_share.py

--- news_advisor_bot_share.py
+++ news_advisor_bot_share.py
@@ -13,8 +13,8 @@ from config import ALPACA_API_KEY, ALPACA_SECRET_KEY, ALPACA_URL, SYMBOLS
 
 warnings.filterwarnings("ignore")
 
-ALPACA_API_KEY = "PKUA5NYJNC3V76V4HKSQ"
-ALPACA_SECRET_KEY = "ORCTeYFP8hBNtj0RlVWfqx0zOVSxTgKcKrdJjvyD"
+ALPACA_API_KEY = "PKSMGL3DXLBX2ROOJ3YF"
+ALPACA_SECRET_KEY = "mL7hpYPj2Bdb7rMPrOwcbYYSuiWIxcKZa9RIMJay"
 ALPACA_URL = "https://paper-api.alpaca.markets"
 
 class NewsAdvisorBot:
