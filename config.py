015e08c4bcb3878dc2e1a8c1257123bdac63f764
rsm-sal072 <140984068+rsm-sal072@users.noreply.github.com>
2024-08-30 02:59:22 -0700

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ import os
 # Explicitly specify the path to the .env file
 load_dotenv(find_dotenv())
 
-ALPACA_API_KEY = PKSMGL3DXLBX2ROOJ3YF
-ALPACA_SECRET_KEY = mL7hpYPj2Bdb7rMPrOwcbYYSuiWIxcKZa9RIMJay
+ALPACA_API_KEY = PK9O1OCIJFWP1Y10UCZH
+ALPACA_SECRET_KEY = AfyweADMdPUIWcodotseAwxJbdbhlJpn4xkT7wP6
 ALPACA_URL = https://paper-api.alpaca.markets
 
 # Stock symbols to trade
